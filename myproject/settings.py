2e54991d504b8ebd591f82f0c14cfb2a32f737bf
dnamgyal <don.namgyal@hotmail.com>
2018-03-10 20:31:27 -0500

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SECRET_KEY = config("SECRET_KEY")
 
